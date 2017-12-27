38b3470d93c9f483380f75f3f9e2316cbfdc048d
Dmitry Soshnikov <dmitrys@fb.com>
2017-12-27 13:09:08 -0800

    Emulator: implement Or8Way gate

---
+++ src/emulator/hardware/builtin-gates/Or8Way.js
@@ -0,0 +1,66 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2017-present Dmitry Soshnikov <dmitry.soshnikov@gmail.com>
+ */
+
+'use strict';
+
+const BuiltInGate = require('../BuiltInGate');
+
+const {int16Table} = require('../../../util/typed-numbers');
+
+/**
+ * Canonical truth table for the `Or8Way` gate.
+ *
+ * Note: for PinBus instances use a subset of the testing table.
+ */
+const TRUTH_TABLE = int16Table([
+  {in: 0b00000000, out: 0b0},
+  {in: 0b11111111, out: 0b1},
+  {in: 0b00010000, out: 0b1},
+  {in: 0b00000001, out: 0b1},
+  {in: 0b00100110, out: 0b1},
+]);
+
+/**
+ * Or of 8 inputs into 1 output.
+ * out=1 if one or more of the inputs is 1 and 0 otherwise.
+ */
+class Or8Way extends BuiltInGate {
+  /**
+   * IN in[8];
+   * OUT out;
+   *
+   * out = (in[0] | in[1] | ... | in[7])
+   *
+   * Abstract:
+   *
+   *   Or(a=in[0], b=in[1], out=out1);
+   *   Or(a=out1, b=in[2], out=out2);
+   *   ...
+   */
+  eval() {
+    const _in = this.getInputPins()[0].getValue();
+
+    this.getOutputPins()[0].setValue(_in === 0 ? 0 : 1);
+  }
+}
+
+/**
+ * Specification of the `Or16` gate.
+ */
+Or8Way.Spec = {
+  description: 'Or | of 8 inputs into 1 output.',
+
+  inputPins: [
+    {name: 'in', size: 8},
+  ],
+
+  outputPins: [
+    'out',
+  ],
+
+  truthTable: TRUTH_TABLE,
+};
+
+module.exports = Or8Way;
---
+++ src/emulator/hardware/builtin-gates/__tests__/Or8Way-test.js
@@ -0,0 +1,16 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2017-present Dmitry Soshnikov <dmitry.soshnikov@gmail.com>
+ */
+
+'use strict';
+
+const Or8Way = require('../Or8Way');
+const GateTestUtil = require('../../gate-test-util');
+
+describe('Or8Way', () => {
+  it('Or8Way interface', () => {
+    expect(() => GateTestUtil.autoTestGate(Or8Way))
+      .not.toThrow();
+  });
+});
