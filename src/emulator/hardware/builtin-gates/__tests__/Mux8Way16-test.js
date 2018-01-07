8dbdca75c4dcb98cad7b288544eb24233acc5e78
Dmitry Soshnikov <dmitrys@fb.com>
2018-01-06 23:14:15 -0800

    Emulator: implement Mux8Way16 gate

---
+++ src/emulator/hardware/builtin-gates/__tests__/Mux8Way16-test.js
@@ -0,0 +1,16 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2017-present Dmitry Soshnikov <dmitry.soshnikov@gmail.com>
+ */
+
+'use strict';
+
+const Mux8Way16 = require('../Mux8Way16');
+const GateTestUtil = require('../../gate-test-util');
+
+describe('Mux8Way16', () => {
+  it('Mux8Way16 interface', () => {
+    expect(() => GateTestUtil.autoTestGate(Mux8Way16))
+      .not.toThrow();
+  });
+});
