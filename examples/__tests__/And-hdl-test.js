d8099dab9eec9cd1d812e78d254a181b3b6201f5
Dmitry Soshnikov <dmitrys@fb.com>
2018-02-05 10:38:47 -0800

    Add And.hdl test

---
+++ examples/__tests__/And-hdl-test.js
@@ -0,0 +1,60 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2017-present Dmitry Soshnikov <dmitry.soshnikov@gmail.com>
+ */
+
+'use strict';
+
+const HDLClassFactory = require('../../src/emulator/hardware/HDLClassFactory');
+
+describe('And.hdl', () => {
+  it('spec', () => {
+    const And = HDLClassFactory.fromHDLFile(__dirname + '/../And.hdl');
+
+    const spec = And.Spec;
+
+    expect(spec).toEqual({
+      description: `Compiled from HDL composite Gate class "And".`,
+
+      inputPins: [
+        {name: 'a', size: 1},
+        {name: 'b', size: 1},
+      ],
+
+      outputPins: [
+        {name: 'out', size: 1},
+      ],
+
+      internalPins: [
+        {name: 'n', size: 1},
+      ],
+
+      truthTable: [],
+    });
+  });
+
+  it('parts', () => {
+    const and =
+      HDLClassFactory.fromHDLFile(__dirname + '/../And.hdl')
+      .defaultFromSpec();
+
+    const partNames = and.getParts().map(part => part.getClass().name);
+    expect(partNames).toEqual(['Nand', 'Nand']);
+  });
+
+  it('truthTable', () => {
+    const and =
+      HDLClassFactory.fromHDLFile(__dirname + '/../And.hdl')
+      .defaultFromSpec();
+
+    const truthTable = [
+      {a: 0, b: 0, n: 1, out: 0},
+      {a: 0, b: 1, n: 1, out: 0},
+      {a: 1, b: 0, n: 1, out: 0},
+      {a: 1, b: 1, n: 0, out: 1},
+    ];
+
+    const {result} = and.execOnData(truthTable);
+    expect(result).toEqual(truthTable);
+  });
+});
